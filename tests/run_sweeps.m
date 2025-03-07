d9acaa8608cd0332ce3edc22bc3f425c7ef9c932
Matilda Peruzzo <mperuzzo@turing.ac.uk>
2025-03-07 14:55:09 +0000

    changed sweep function - added dataset choice

--- tests/run_sweeps.m
+++ tests/run_sweeps.m
@@ -5,15 +5,42 @@ clc
 
 % Add the directory containing run_model.m to the MATLAB path
 addpath('src');
-rng(12) % set random seed
+dataset = 'pinhasi';
 
-% choose whether to load pinhasi dataset or create a dataset
-active_layers = [1 0 0 1 0];
-cobo = readtable( ...
-     'data/raw/cobo_etal/cobo_etal_data.xlsx');
-parameters = data_prep(100, active_layers, cobo.Latitude, cobo.Longitude, cobo.Est_DateMean_BC_AD_);
-% theory_theta = [0.15 0.05 0.1];
-% parameters = create_dataset(theory_theta, 20, [53.0627, 43.6865]);
+if strcmp(dataset,'pinhasi')
+    % load pinhasi
+    pinhasi = readtable( ...
+        'data/raw/pinhasi/Neolithic_timing_Europe_PLOS.xls');
+
+    pinhasi = pinhasi(pinhasi.Var1 == "SITE",:); %% keep only site rows
+
+    pinhasi = renamevars(pinhasi, {'Latitude', 'Longitude', 'CALC14BP'}, ...
+        {'lat', 'lon', 'bp'});
+
+    pinhasi = pinhasi(:,{'lat', 'lon', 'bp'});
+    pinhasi.bp = 2000 - pinhasi.bp; % from BP to year
+
+    x = pinhasi.lat;
+    y = pinhasi.lon;
+    t = pinhasi.bp;
+
+elseif strcmp(dataset,'cobo')
+    % LOAD COBO et al
+
+    cobo = readtable( ...
+         'data/raw/cobo_etal/cobo_etal_data.xlsx');
+
+    x = cobo.Latitude;
+    y = cobo.Longitude;
+    t = cobo.Est_DateMean_BC_AD_;
+end
+
+active_layers = [1 0 0 1 0 0 0 0];
+parameters = data_prep(50, active_layers, x,y,t);
+
+if strcmp(dataset,'cobo')
+    parameters.A(76,39,46) = true;
+end
 % data_prep creates parameters struct with the following fields:
 % parameters.A - initial matrix
 % parameters.T - number of time steps
@@ -29,15 +56,17 @@ parameters = data_prep(100, active_layers, cobo.Latitude, cobo.Longitude, cobo.E
 % parameters.lon - first and last longitude
 % parameters.n - number of averages
 
+
+
 %%
 
-ranges = [[-2, -0.0]; [0.0,2.0]];
-n_points = 51;
+ranges = [[-2, -0.0]; [-2.0,0.0]];
+n_points = 21;
 [theta_min, on_edge, min_error, errors] = sweep(ranges, n_points, 0, parameters);
 theta_0 = linspace(ranges(1,1), ranges(1,2), n_points);
 theta_1 = linspace(ranges(2,1), ranges(2,2), n_points);
 all_errors = reshape(errors, [n_points,n_points]);
 % 
-save("cobo_dataset_av_hydro_2_start.mat","all_errors","theta_0","theta_1",'-mat')
+save("av_hydro.mat","all_errors","theta_0","theta_1",'-mat')
 disp("Done!")
 
