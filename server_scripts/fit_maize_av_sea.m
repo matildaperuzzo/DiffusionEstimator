1d056c07b454ae1cb8526991353f376dca298423
Matilda Peruzzo <matilda.peruzzo@gmail.com>
2026-03-17 12:00:25 +0000

    fixed directory issue

--- server_scripts/fit_maize_av_sea.m
+++ server_scripts/fit_maize_av_sea.m
@@ -3,6 +3,7 @@ clc;
 
 script_dir = fileparts(mfilename('fullpath'));
 repo_root = fileparts(script_dir);
+cd(repo_root);
 addpath(fullfile(repo_root, 'src'));
 
 fit_model('maize', {'av', 'sea'}, true);
